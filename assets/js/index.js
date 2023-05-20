c2bf66fabda7282a11db46aa4eec0d059d4d9876
Mohamed-ncib19 <mohamedncib900@gmail.com>
2023-05-20 17:58:59 +0100

    quick updates and deploy

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,6 +8,9 @@ let conditionResult = document.querySelector('#condition')
 let humidityPercentageDisplay = document.querySelector('#humidityPercentage')
 let windspeedDisplay = document.querySelector('#windspeed')
 
+//input field
+let inputSearch = document.querySelector('#search')
+
 //search button
 let searchButton = document.querySelector('#search-button')
 
@@ -15,20 +18,23 @@ let weather = {
     key:"dfd0e437b75148bc8ac02319231004",
 
     fetchData :function (city){
-    try {
+    
         let api = 'https://api.weatherapi.com/v1/current.json?key='+this.key+'&q='+city+'&aqi=yes'
         fetch(api)
         .then((response)=>response.json())
         
-        .then((data)=>weather.displayWeather(data))
-        console.log("success")
+        .then((data)=>{weather.displayWeather(data)
+        console.log("success")})
         
-    } catch (error) {
-    console.log("error")
-    alert('check the country name again')
-}
-    
+        .catch((err)=>{
+            console.log(err)
+            alert('no matchmaking name');
+            
+        })
+        
     },
+    
+    
     displayWeather : function(data){
         
         console.log(data)
@@ -37,7 +43,7 @@ let weather = {
         let date = data.location['localtime']
         let condition = data.current['condition'].text
         let icon = data.current['condition'].icon
-        let temp_f = data.current['temp_f']
+        
         let humiditypercentage = data.current['humidity']
         let windSpeed = data.current['wind_mph']
         //getting time part to check if its  or AM
@@ -63,6 +69,7 @@ let weather = {
 
 
 };
+
 searchButton.addEventListener('click',()=>{
     weather.fetchData(document.querySelector('#search').value)
     
